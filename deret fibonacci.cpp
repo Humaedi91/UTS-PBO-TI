c8a074a222206909540974f91b3a255f839ddfb4
Humaedi91 <83716103+Humaedi91@users.noreply.github.com>
2021-05-06 15:52:29 +0700

    Add files via upload
    uts humaedi 1101201094, TI

---
+++ deret fibonacci.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int f1,f2,jml;
+	cout << "jml angka=";cin >> jml;
+	cout << "f1=";cin>> f1;
+	cout << "f2=";cin>> f2;
+	int temp = 0;
+	cout << f1 <<",";
+	cout << f2 <<",";
+	for(int a = 3; a <=jml; a++){
+		temp = f1 + f2;
+		cout << temp;
+		if(a !=jml){
+			cout << ",";
+		}
+		f1 = f2;
+		f2 = temp;
+	}
+}
